ddbafe0e246023225612a6ad81fbf45aa261bf78
sanjay D <sanjuop@gmail.com>
2021-01-28 22:47:18 +0530

    profile and department model

--- avocadobites/userprofile/admin.py
+++ avocadobites/userprofile/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from userprofile.models import Department, Profile
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Department)
--- avocadobites/userprofile/models.py
+++ avocadobites/userprofile/models.py
@@ -1,35 +1,48 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+
+class Department(models.Model):
+    department = models.CharField(max_length=200)
+    short_name = models.CharField(max_length=20, null=True, blank=True)
+    department_type = models.CharField(max_length=20)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def __str__(self):
+        return self.department
+
 def user_image_path(instance, filename):
     # image file will be uploaded to MEDIA_ROOT/<username>/<filename>
     return '{0}/{1}'.format(instance.employee_code, filename)
 
 # Create your models here.
-class profile(models.Model):
+class Profile(models.Model):
 
     MARITAL_STATUS_CHOICES = (
-        (1, 'Single'),
-        (2, 'Married'),
-        (3, 'Widowed'),
-        (4, 'Divorced'),
-        (5, 'Engaged')
+        ("1", 'Single'),
+        ("2", 'Married'),
+        ("3", 'Widowed'),
+        ("4", 'Divorced'),
+        ("5", 'Engaged')
     )
 
     GENDER_CHOICES = (
-        (1, 'MALE'),
-        (2, 'FEMALE'),
-        (3, 'OTHER')
+        ("1", 'MALE'),
+        ("2", 'FEMALE'),
+        ("3", 'OTHER')
 
     )
 
     
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to=user_image_path, blank=True)
+    image = models.ImageField(upload_to=user_image_path, null=True, blank=True,)
     employee_code = models.CharField(max_length=20, unique=True)
-    middle_name = models.CharField(max_length=200)
-    department = models.ForeignKey
+    middle_name = models.CharField(max_length=200, null=True, blank=True,)
+    department = models.ForeignKey(Department, default=1, verbose_name='emp_department', on_delete=models.SET_DEFAULT)
     designation = models.CharField(max_length=50)
+    skill_level = models.CharField(max_length=10)
     date_of_birth = models.DateField()
     birth_place = models.CharField(max_length=200)
     current_location = models.CharField(max_length=20)
@@ -40,15 +53,26 @@ class profile(models.Model):
     permanent_address = models.TextField(null=True, blank = True)
     current_address = models.TextField(null=True, blank = True)
     personal_phone_number = models.CharField(max_length=50)
-    altername_phone_number = models.CharField(max_length=50)
-    pan_number = models.CharField(max_length=50)
-    passpart_number = models.CharField(max_length=50)
-    father_name = models.CharField(max_length=300)
-    mother_name = models.CharField(max_length=300)
-    marital_status = models.CharField(max_length=300, choices=MARITAL_STATUS_CHOICES)
-    spouse_name = models.CharField(max_length=300)
-    emergency_contact_number_1 = models.CharField(max_length=40)
-    emergency_contact_number_2 = models.CharField(max_length=40)
+    altername_phone_number = models.CharField(max_length=50, null=True, blank=True,)
+    pan_number = models.CharField(max_length=50, null=True, blank=True,)
+    passport_number = models.CharField(max_length=50, null=True, blank=True,)
+    father_name = models.CharField(max_length=300, null=True, blank=True,)
+    mother_name = models.CharField(max_length=300, null=True, blank=True,)
+    marital_status = models.CharField(max_length=300, choices=MARITAL_STATUS_CHOICES, null=True, blank=True,)
+    spouse_name = models.CharField(max_length=300, null=True, blank=True,)
+    emergency_contact_number_1 = models.CharField(max_length=40, null=True, blank=True,)
+    emergency_contact_number_2 = models.CharField(max_length=40, null=True, blank=True,)
+    project_assigned = models.CharField(max_length=40, null=True, blank=True,)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def get_full_name(self):
+        return self.user.full_name + ' ' + self.user.last_name
+
+    def __str__(self):
+        return self.user.username
+
+
 
 
 
